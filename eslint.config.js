afedb2ee166fb5c32d86a03bcbb6fb77fabf3b02
Sudhan <sudhanplayz@gmail.com>
2025-09-01 21:30:17 +0530

    W

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+const js = require('@eslint/js');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.js', '**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      'semi': ['error', 'always'],
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'no-undef': 'off', // Disable for CommonJS modules
+      '@typescript-eslint/no-require-imports': 'off', // Allow require() for CommonJS
+    },
+  },
+];
